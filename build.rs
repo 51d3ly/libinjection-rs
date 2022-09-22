e449505dc6a0ce79a377ddd46577ec45a20e5209
Adrien Jonquet <adrien.jonquet@gmail.com>
2022-09-22 15:09:52 +0200

    fix git credentials macos

--- build.rs
+++ build.rs
@@ -2,20 +2,56 @@ extern crate bindgen;
 extern crate git2;
 extern crate regex;
 
-use git2::Repository;
+use git2::{Repository, RemoteCallbacks};
 use regex::Regex;
 use std::env;
 use std::fs::remove_dir_all;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-const LIBINJECTION_URL: &'static str = "https://github.com/client9/libinjection";
+const LIBINJECTION_URL: &'static str = "git@github.com:libinjection/libinjection.git";
+// const LIBINJECTION_URL: &'static str = "https://github.com/client9/libinjection";
+
+// https://github.com/libinjection/libinjection.git
 const BUILD_DIR_NAME: &'static str = "libinjection";
 
 fn clone_libinjection(build_dir: &Path, version: &str) -> Option<()> {
-    let repo = Repository::clone(LIBINJECTION_URL, build_dir).ok()?;
-    let rev = repo.revparse_single(version).ok()?;
-    repo.set_head_detached(rev.id()).ok()
+    let ssh_key_path = env::var("GIT_SSH_KEY_PATH")
+    .expect("No GIT_SSH_KEY_PATH environment variable found");
+
+
+
+    let mut callbacks = git2::RemoteCallbacks::new();
+    callbacks.credentials(|_url, username_from_url, _allowed_types| {
+        git2::Cred::ssh_key(
+            username_from_url.unwrap(),
+          None,
+          std::path::Path::new(&ssh_key_path),
+          None,
+        )
+      });
+    
+    let mut opts = git2::FetchOptions::new();
+    opts.remote_callbacks(callbacks);
+    opts.download_tags(git2::AutotagOption::All);
+
+    let mut builder = git2::build::RepoBuilder::new();
+    builder.fetch_options(opts);
+    builder.branch("main");
+
+    match builder.clone(LIBINJECTION_URL, build_dir)  {
+
+        Ok(repo) => {
+            println!("{:?}",repo.path() );
+            let rev = repo.revparse_single(version).ok()?;
+            repo.set_head_detached(rev.id()).ok()
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            None
+        }
+    }
+   
 }
 
 fn run_make(rule: &str, cwd: &Path) -> bool {
@@ -50,6 +86,8 @@ fn fix_python_version() -> Option<()> {
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let mut build_parent_dir = out_path.join(BUILD_DIR_NAME);
+
+    println!("{:?}",out_path );
 
     let _ = remove_dir_all(build_parent_dir.as_path());
 
